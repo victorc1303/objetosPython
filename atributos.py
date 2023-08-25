486984ffc15d335dcb5905327089e60ec1e5c1b2
victorc1303 <vctes.duran@gmail.com>
2023-08-25 18:00:34 -0500

    seguro de este archivo

---
+++ atributos.py
@@ -0,0 +1,13 @@
+﻿##propiedades de instancia sin parentesis
+
+class Celular:
+## metodo constructor 
+    def __init__(self,marca,modelo,camara):#constructor
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+celular1 = Celular("nokia","1100","40mpx")#Objetos
+celular2 = Celular("samsung","s13","50mpx")#Objetos
+
+print(celular1.marca)
+print(celular2.modelo)
